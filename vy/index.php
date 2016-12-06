23e7e6365e9a199f46d8764b488f81e830733efe
TuongVyNguyen <nguyenthituongvy13cntt@gmail.com>
2016-12-06 11:08:18 +0700

    baitap
    3 bài tập

---
+++ vy/index.php
@@ -0,0 +1,9 @@
+<h1>Hello from Dr. Chuck's HTML Page</h1>
+<p>
+<?php
+   echo "Hi there.\n";
+   $answer = 6 * 7;
+   echo "The answer is $answer, what was the question again?\n";
+?>
+</p>
+<p>Yes another paragraph.</p>
