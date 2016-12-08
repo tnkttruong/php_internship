c3e55781d6d3f47490acd49557edcb90a5bd502f
TuongVyNguyen <nguyenthituongvy13cntt@gmail.com>
2016-12-08 17:15:22 +0700

    Bài tập

---
+++ vy/field1.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FIELD</title>
+       <style>
+           *
+           {
+               box-sizing: border-box;
+           }
+           .thethan
+           {
+               display: block;
+               width: 100%;
+
+               padding: 20px;
+           }
+           .thekhung
+           {
+               display: block;
+               width: 100%;
+
+               border: 1px solid #eef1f5;
+           }
+           .thethan1
+           {
+               display: table;
+               width: 100%;
+               height: 80px;
+               border-bottom: 1px solid #ececec;
+               border-radius: 20px;
+
+               line-height: 60px;
+           }
+           .theleft
+           {
+               float: left;
+               width: 30%;
+               height: 80px;
+               /*text-align: center;*/
+               /*line-height: 60px;*/
+               padding: 10px 10px;
+               background-color: #eef1f5;
+           }
+           .thechu
+           {
+               display: inline-table;
+               font-size: 20px;
+               font-weight: bold;
+                padding-right: 20px;
+           }
+           .thebutton
+           {
+               display: inline-table;
+               width: 60px;
+               background-color: red;
+               text-align: center;
+               line-height: 30px;
+           }
+           .button_vy
+           {
+               font-size: 15px;
+               color: white;
+           }
+           .theright
+           {
+               float: left;
+               width: 70%;
+               height: 80px;
+               background-color: white;
+               padding-top: 10px;
+               padding-right: 10px;
+               padding-left: 10px;
+               padding-bottom: 30px;
+
+           }
+
+       </style>
+    </head>
+    <body>
+        <div class="thethan">
+            <div class="thekhung">
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Nguyễn Thị Tường Vy</div>
+                        <div class="thebutton">
+                            <div class="button_vy">Vy</div>
+                        </div>
+                    </div>
+                    <div class="theright">
+
+                            <input style="width: 100%;height: 80%;" type="text" value="" placeholder="vui lòng nhập vào đây">
+
+                    </div>
+                </div>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Nguyễn Thị Tường Vy</div>
+                        <div class="thebutton">
+                            <div class="button_vy">Vy</div>
+                        </div>
+                    </div>
+                    <div class="theright">
+                       <select style="width: 100%;height: 80%;border: 1px solid #2aabd2">
+                            <option>vy</option>
+                            <option>vy</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Nguyễn Tường </div>
+                        <div class="thebutton">
+                            <div class="button_vy">Vy</div>
+                        </div>
+                    </div>
+                    <div class="theright">
+                        <input style="width: 100%;height: 80%;" type="text" value="" placeholder="vui lòng nhập vào đây">
+                    </div>
+                </div>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Tường Vy</div>
+                        <div class="thebutton">
+                            <div class="button_vy">Vy</div>
+                        </div>
+                    </div>
+                    <div class="theright">
+                        <input style="width: 100%;height: 80%;" type="text" value="" placeholder="vui lòng nhập vào đây">
+                    </div>
+                </div>
+
+                <textarea style="width: 100%;height: 60px;border: 1px solid red"></textarea>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Tường Vy</div>
+                        <div class="thebutton">
+                            <div class="button_vy">Vy</div>
+                        </div>
+                    </div>
+                    <div class="theright">
+                        <input style="display: inline;line-height: 60px;" type="radio">
+                        <div class="thechu">S</div>
+                        <input style="display: inline;line-height: 60px;" type="radio">
+                        <div class="thechu">A</div>
+                        <input style="display: inline;line-height: 60px;" type="radio">
+                        <div class="thechu">C</div>
+                    </div>
+                </div>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Tường Vy</div>
+
+                    </div>
+                    <div class="theright">
+                        <input style="display: inline;line-height: 60px;" type="checkbox">
+                        <div class="thechu">NSC</div>
+
+                    </div>
+                </div>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Tường Vy</div>
+
+                    </div>
+                    <div class="theright">
+                        <input style="display: inline;line-height: 60px;" type="checkbox">
+                        <div class="thechu">NSC</div>
+
+                    </div>
+                </div>
+                <div class="thethan1">
+                    <div class="theleft">
+                        <div class="thechu">Tường Vy</div>
+
+                    </div>
+                    <div class="theright">
+                        <input style="display: inline;line-height: 60px;" type="checkbox">
+                        <div class="thechu">NSC</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ vy/table.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TABLE</title>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+            .khung
+            {
+                display: block;
+                width: 1200px;
+                height: 300px;
+            }
+            .thebutton
+            {
+                display: inline-table;
+                width: 60px;
+                background-color: red;
+                text-align: center;
+                line-height: 30px;
+                margin-left: 20px;
+            }
+            .thebutton2
+            {
+                display: inline-table;
+                width: 60px;
+                background-color: #0044cc;
+                text-align: center;
+                line-height: 30px;
+                margin-left: 20px;
+            }
+            .thebutton1
+            {
+                display: inline-table;
+                width: 60px;
+                background-color: #1b6d85;
+                text-align: center;
+                line-height: 30px;
+                margin-left: 20px;
+            }
+            .button_vy
+            {
+                font-size: 15px;
+                color: white;
+            }
+            .thechu
+            {
+                display: inline-table;
+                font-size: 20px;
+                font-weight: bold;
+                padding-right: 20px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="khung">
+        <table style="height: 100%;width: 100%;" border="1">
+            <tr>
+                <th>STT</th>
+                <th>Diện thoại</th>
+                <th>Lớp</th>
+                <th>Khoa</th>
+                <th>Ngày</th>
+                <th> </th>
+            </tr>
+            <tr>
+                <td >
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                </td>
+                <td style="color: #4cff00">01231</td>
+                <td>A</td>
+                <td>
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                    <div class="thechu">Nguyễn thị tường vy</div>
+                </td>
+                <td>09/05/1994 ~ 17/05/1995</td>
+                <td>
+                    <div class="thebutton1">
+                        <div class="button_vy">Thêm</div>
+                    </div>
+                    <div class="thebutton">
+                        <div class="button_vy">Sửa</div>
+                    </div>
+                    <div class="thebutton2">
+                        <div class="button_vy">Xóa</div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td >
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                </td>
+                <td style="color: #4cff00">01231</td>
+                <td>A</td>
+                <td>
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                    <div class="thechu">Nguyễn thị tường vy</div>
+                </td>
+                <td>09/05/1994 ~ 17/05/1995</td>
+                <td>
+                    <div class="thebutton1">
+                        <div class="button_vy">Thêm</div>
+                    </div>
+                    <div class="thebutton">
+                        <div class="button_vy">Sửa</div>
+                    </div>
+                    <div class="thebutton2">
+                        <div class="button_vy">Xóa</div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td >
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                </td>
+                <td style="color: #4cff00">01231</td>
+                <td>A</td>
+                <td>
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                    <div class="thechu">Nguyễn thị tường vy</div>
+                </td>
+                <td>09/05/1994 ~ 17/05/1995</td>
+                <td>
+                    <div class="thebutton1">
+                        <div class="button_vy">Thêm</div>
+                    </div>
+                    <div class="thebutton">
+                        <div class="button_vy">Sửa</div>
+                    </div>
+                    <div class="thebutton2">
+                        <div class="button_vy">Xóa</div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td >
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                </td>
+                <td style="color: #4cff00">01231</td>
+                <td>A</td>
+                <td>
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                    <div class="thechu">Nguyễn thị tường vy</div>
+                </td>
+                <td>09/05/1994 ~ 17/05/1995</td>
+                <td>
+                    <div class="thebutton1">
+                        <div class="button_vy">Thêm</div>
+                    </div>
+                    <div class="thebutton">
+                        <div class="button_vy">Sửa</div>
+                    </div>
+                    <div class="thebutton2">
+                        <div class="button_vy">Xóa</div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td >
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                </td>
+                <td style="color: #4cff00">01231</td>
+                <td>A</td>
+                <td>
+                    <div class="thebutton">
+                        <div class="button_vy">Số một</div>
+                    </div>
+                    <div class="thechu">Nguyễn thị tường vy</div>
+                </td>
+                <td>09/05/1994 ~ 17/05/1995</td>
+                <td>
+                    <div class="thebutton1">
+                        <div class="button_vy">Thêm</div>
+                    </div>
+                    <div class="thebutton">
+                        <div class="button_vy">Sửa</div>
+                    </div>
+                    <div class="thebutton2">
+                        <div class="button_vy">Xóa</div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    </body>
+</html>
