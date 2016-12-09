c3e3eb46a1c6220dab3bd45c1e1c55842e0b01a9
LeeHoai <leehoai4922@gmail.com>
2016-12-09 22:14:02 +0700

    update

--- hoai_php/createhtml.php
+++ hoai_php/createhtml.php
@@ -3,61 +3,75 @@
 <head>
     <meta charset="UTF-8">
     <title>Create some HTML</title>
-<style >
-    table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 60%;
-    }
-    td {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 15px;
-    }
-td .name{background: #99ccff;}
-</style>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 60%;
+        }
+
+        td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 15px;
+        }
+
+        .span {
+            color: white;
+            background-color: #de797b;
+        }
+
+        .color {
+            background: #eef1f5;
+        }
+
+        .input {
+            width: 500px;
+            height: 20px;
+        }
+    </style>
 </head>
 <body>
 <table>
-        <tr>
-            <td class="name"style="background: #eef1f5;"><label>Tên nguyên vật liệu <span style="color: white;background-color: #de797b">Bắt buộc</span></label></td>
-                <td><select style="width: 500px;height: 20px;">
-                        <option value="volvo">ABCDB</option>
-                        <option value="saab">CGDKKS</option>
-                    </select></td>
-
-        </tr>
-        <tr>
-            <td style="background: #eef1f5;"><label>Nhà cung cấp <span style="color: white;background-color: #de797b">Bắt buộc</span></label></td>
-            <td><input style="width: 500px;height: 20px;" type="text" name="gread" placeholder="Nhập tên nhà cung cấp"></td>
-        </tr>
-        <tr>
-            <td style="background: #eef1f5;"><label>Ngày nhập <span style="color: white;background-color: #de797b">Bắt buộc</span></label></td>
-            <td><input style="width: 500px;height: 20px;" type="text" name="address" placeholder="nhập ngày(yyyy/mm/dd)"></td>
-        </tr>
-        <tr>
-            <td style="background: #eef1f5;"><label>Đánh giá <span style="color: white;background-color: #de797b">Bắt buộc</span></label></td>
-            <td><input type="radio" name="radio" value="1">A
-                <input type="radio" name="radio" value="2">B
-                <input type="radio" name="radio" value="3">C
-
-            </td>
-
-
-        </tr>
     <tr>
-        <td style="background: #eef1f5;">Sách</td>
-        <td><input type="checkbox" name="check1">Ngày này năm ấy</td>
+        <td class="color"><label>Tên nguyên vật liệu <span
+                        class="span">Bắt buộc</span></label></td>
+        <td><select class="input">
+                <option value="volvo">ABCDB</option>
+                <option value="saab">CGDKKS</option>
+            </select>
+        </td>
     </tr>
     <tr>
-        <td style="background: #eef1f5;">Truyện</td>
-        <td><input type="checkbox" name="check1">Harry Potter</td>
+        <td class="color"><label>Nhà cung cấp <span class="span">Bắt buộc</span></label>
+        </td>
+        <td><input class="input" type="text" name="gread" placeholder="Nhập tên nhà cung cấp"></td>
     </tr>
     <tr>
-        <td style="background: #eef1f5;">Tạp chí</td>
-        <td><input type="checkbox" name="check1">Đời sống</td>
+        <td class="color"><label>Ngày nhập <span
+                        class="span">Bắt buộc</span></label></td>
+        <td><input class="input" type="text" name="address" placeholder="nhập ngày(yyyy/mm/dd)">
+        </td>
+    </tr>
+    <tr>
+        <td class="color"><label>Đánh giá <span class="span">Bắt buộc</span></label></td>
+        <td><input type="radio" name="radio" value="1">A
+            <input type="radio" name="radio" value="2">B
+            <input type="radio" name="radio" value="3">C
+        </td>
+    </tr>
+    <tr>
+        <td class="color">Sách</td>
+        <td><input type="checkbox" name="book">Ngày này năm ấy</td>
+    </tr>
+    <tr>
+        <td class="color">Truyện</td>
+        <td><input type="checkbox" name="story">Harry Potter</td>
+    </tr>
+    <tr>
+        <td class="color">Tạp chí</td>
+        <td><input type="checkbox" name="magazine">Đời sống</td>
     </tr>
 </table>
-
 </body>
 </html>
--- hoai_php/createtable.php
+++ hoai_php/createtable.php
@@ -1,84 +1,130 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 70%;
-}
-
-td, th {
-    border: 1px solid #e8ecf1;
-    text-align: left;
-    padding: 15px;
-}
-
-tr:nth-child(even) {
-background-color: #fbfcfd;
-}
-.button {
-	background-color: #4CAF50; /* Green */
-        border: none;
-        color: white;
-        padding: 10px 14px;
-        text-align: center;
-        font-size: 13px;
-        margin: 4px 2px;
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 70%;
         }
-.button1 {background-color: #5dc1cf;}
-.button2 {background-color: #489ad9}
-.button3 {background-color:#d86362;}
-</style>
+
+        td, th {
+            border: 1px solid #e8ecf1;
+            text-align: left;
+            padding: 15px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #fbfcfd;
+        }
+
+        .button {
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            padding: 10px 14px;
+            text-align: center;
+            font-size: 13px;
+            margin: 4px 2px;
+        }
+
+        .add {
+            background-color: #5dc1cf;
+        }
+
+        .delete {
+            background-color: #489ad9;
+        }
+
+        .update {
+            background-color: #d86362;
+        }
+
+        .span {
+            color: white;
+        }
+
+        .purple {
+            background-color: #847ca8;
+        }
+
+        .red {
+            background-color: #d55450;
+        }
+
+        .blue {
+            background-color: #4f7bbc;
+        }
+
+        .gray {
+            background-color: #bbc3cf;
+        }
+    </style>
 </head>
 <body>
-
 <table>
-  <tr>
-    <th>Tên nguyên liệu</th>
-    <th>ID</th>
-    <th>Nhà cung cấp</th>
-	<th>Ngày nhập</th>
-	<td></td>
-	
-  </tr>
-  <tr>
-    <td><span style="color: white;background-color: #847ca8">Cà phê chuyên biệt</span></td>
-    <td>01</td>
-	<td><span style="color: white;background-color: #bbc3cf">Cà phê Trung Nguyên</span></td>
-    <td>2016/12/3</td>
-	<td><button class="button button1">add</button><button  class="button button2">delete</button><button class="button button3">update</button></td>
-  </tr>
-  <tr>
-    <td><span style="color: white;background-color: #d55450">Cà phê Rang xay</span></td>
-    <td>02</td>
-    <td><span style="color: white;background-color: #6b9edd">Cà Phê G7</span></td>
-	<td>2016/3/2</td>
-	<td><button class="button button1">add</button><button  class="button button2">delete</button><button class="button button3">update</button></td>
-  </tr>
-  <tr>
-    <td><span style="color: white;background-color: #d55450">Cà phê Culi</span></td>
-    <td>03</td>
-    <td><span style="color: white;background-color: #bbc3cf">Cà phê Trung Nguyên</span></td>
-	<td>2016/3/2</td>
-	<td><button class="button button1">add</button><button  class="button button2">delete</button><button class="button button3">update</button></td>
-  </tr>
-  <tr>
-    <td><span style="color: white;background-color: #4f7bbc">Cà phê Espresso</span></td>
-    <td>04</td>
-    <td><span style="color: white;background-color: #bbc3cf">Cà phê Trung Nguyên</span></td>
-	<td>2016/3/2</td>
-	<td><button class="button button1">add</button><button  class="button button2">delete</button><button class="button button3">update</button></td>
-  </tr>
-  <tr>
-    <td><span style="color: white;background-color: #4f7bbc">Cà phê Hòa tan</span></td>
-    <td>05</td>
-    <td><span style="color: white;background-color: #6b9edd">Cà Phê G7</span></td>
-	<td>2016/3/2</td>
-	<td><button class="button button1">add</button><button  class="button button2">delete</button><button class="button button3">update</button></td>
-  </tr>
-  
+    <tr>
+        <th>Tên nguyên liệu</th>
+        <th>ID</th>
+        <th>Nhà cung cấp</th>
+        <th>Ngày nhập</th>
+        <td></td>
+    </tr>
+    <tr>
+        <td><span class="span purple">Cà phê chuyên biệt</span></td>
+        <td>01</td>
+        <td><span class="span gray">Cà phê Trung Nguyên</span></td>
+        <td>2016/12/3</td>
+        <td>
+            <button class="button add">add</button>
+            <button class="button delete">delete</button>
+            <button class="button update">update</button>
+        </td>
+    </tr>
+    <tr>
+        <td><span class="span red">Cà phê Rang xay</span></td>
+        <td>02</td>
+        <td><span class="span blue">Cà Phê G7</span></td>
+        <td>2016/3/2</td>
+        <td>
+            <button class="button add">add</button>
+            <button class="button delete">delete</button>
+            <button class="button update">update</button>
+        </td>
+    </tr>
+    <tr>
+        <td><span class="span red">Cà phê Culi</span></td>
+        <td>03</td>
+        <td><span class="span gray">Cà phê Trung Nguyên</span></td>
+        <td>2016/3/2</td>
+        <td>
+            <button class="button add">add</button>
+            <button class="button delete">delete</button>
+            <button class="button update">update</button>
+        </td>
+    </tr>
+    <tr>
+        <td><span class="span blue">Cà phê Espresso</span></td>
+        <td>04</td>
+        <td><span class="span gray">Cà phê Trung Nguyên</span></td>
+        <td>2016/3/2</td>
+        <td>
+            <button class="button add">add</button>
+            <button class="button delete">delete</button>
+            <button class="button update">update</button>
+        </td>
+    </tr>
+    <tr>
+        <td><span class="span blue">Cà phê Hòa tan</span></td>
+        <td>05</td>
+        <td><span class="span blue">Cà Phê G7</span></td>
+        <td>2016/3/2</td>
+        <td>
+            <button class="button add">add</button>
+            <button class="button delete">delete</button>
+            <button class="button update">update</button>
+        </td>
+    </tr>
 </table>
-
 </body>
 </html>
